8177b153b044e85d91a2f1806272271e863ca9b0
dejanmccreery <108457094+dejanmccreery@users.noreply.github.com>
2022-08-04 11:33:57 +0100

    more premade code from Dejan's local computer

---
+++ .idea/main/java/com.hfplatinum/controllers/DeliveryController.java
@@ -0,0 +1,84 @@
+package com.qa.newguinea.main.controllers;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class DeliveryController implements CrudController<Delivery> {
+
+    Scanner scan = new Scanner(System.in);
+
+    DeliveryDAO dao = new DeliveryDAO();
+
+    @Override
+    public List<Delivery> readAll() {
+        List<Delivery> list = new ArrayList<Delivery>();
+        list = dao.readAll();
+        System.out.println(list);
+        return null;
+    }
+
+    @Override
+    public Delivery read() {
+        System.out.println("Please input delivery ID:");
+        int deliveryid = scan.nextInt();
+        Delivery d = dao.read(deliveryid);
+        System.out.println(d);
+        return null;
+    }
+
+    @Override
+    public Delivery readLatest() {
+        Delivery d = dao.readLatest();
+        System.out.println(d);
+        return null;
+    }
+
+    @Override
+    public Delivery create() {
+        System.out.println("Please input order ID:");
+        int orderId = scan.nextInt();
+        System.out.println("Please input delivery date:");
+        String dateInString = scan.nextLine();
+        Date date = Date.valueOf(dateInString);
+        System.out.println("Please input address:");
+        String deliveryaddress = scan.nextLine();
+        System.out.println("PLease input delivery zone:");
+        String deliveryzone = scan.nextLine();
+        String status = "Awaiting driver";
+        Delivery d = dao.create(new Delivery(orderId, date, deliveryaddress, deliveryzone, 0, status));
+        //create delivery using dao
+        return null;
+    }
+
+    @Override
+    public Delivery update() {
+        System.out.println("Please input delivery ID:");
+        int deliveryid = scan.nextInt();
+        System.out.println("Select option:\nAssign Driver\nUpdate Status");
+        String option = scan.nextLine();
+        switch(option.toLowerCase()) {
+            case "assign driver" :
+                //assign driver id to delivery
+                break;
+            case "update status":
+                System.out.println("Awaiting driver\nDispatched\nDelivered\nRescheduled\nCancelled");
+                break;
+            default:
+                break;
+        }
+        return null;
+    }
+
+    @Override
+    public int delete() {
+        System.out.println("Please input delivery ID:");
+        int id = scan.nextInt();
+        //dao delivery
+        System.out.println("Delivery cancelled.");
+        return 0;
+    }
+
+}
---
+++ .idea/main/java/com.hfplatinum/controllers/OrderController.java
@@ -0,0 +1,85 @@
+package com.qa.newguinea.main.controllers;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.Scanner;
+
+import com.qa.newguinea.main.dao.OrderDAO;
+import com.qa.newguinea.main.persistance.Delivery;
+import com.qa.newguinea.main.persistance.Order;
+
+public class OrderController implements CrudController<Order> {
+
+    Scanner scan = new Scanner(System.in);
+
+    OrderDAO dao = new OrderDAO();
+
+    @Override
+    public List<Order> readAll() {
+        List<Order> list = dao.readAll();
+        System.out.println(list);
+        return list;
+    }
+
+    @Override
+    public Order read() {
+        System.out.println("Please input Order ID:");
+        int orderid = scan.nextInt();
+        Order o = dao.read(orderid);
+        System.out.println(o);
+        return o;
+    }
+
+    @Override
+    public Order readLatest() {
+        Order o = dao.readLatest();
+        System.out.println(o);
+        return o;
+    }
+
+    @Override
+    public Order create() {
+        System.out.println("Please input order ID:");
+        int orderId = scan.nextInt();
+        System.out.println("Please input order date:");
+        String dateInString = scan.nextLine();
+        Date date = Date.valueOf(dateInString);
+        System.out.println("Please input address:");
+        String orderaddress = scan.nextLine();
+        System.out.println("PLease input delivery zone:");
+        String deliveryzone = scan.nextLine();
+        String status = "Awaiting driver";
+        Order o = dao.create(new Order(orderId, date, orderaddress));
+        return null;
+    }
+
+    @Override
+    public Order update() {
+        System.out.println("Please input order ID:");
+        int orderid = scan.nextInt();
+        System.out.println("please input order date:");
+        String dateInString = scan.nextLine();
+        Date date = Date.valueOf(dateInString);
+        System.out.println("Please input address:");
+        String orderaddress = scan.nextLine();
+        return null;
+    }
+
+    @Override
+    public int delete() {
+        System.out.println("Please input order ID:");
+        int id = scan.nextInt();
+        int result = dao.delete(id);
+
+        if(result == 1) {
+            System.out.println("Order cancelled.");
+        }
+        else {
+            System.out.println("Error! PLease try again.");
+        }
+
+        return 0;
+    }
+
+}
+
